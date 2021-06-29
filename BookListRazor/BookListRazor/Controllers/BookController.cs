f33f8c7218351a77f96ce9a3200fdbcf8e8306cd
Zhivko <fbinnzhivko@gmail.com>
2021-06-29 13:53:25 +0300

    Add project Book List with Razer pages

---
+++ BookListRazor/BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using BookListRazor.Data;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Books.ToList() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book bookFromDb = await _db.Books.FirstOrDefaultAsync(book => book.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+    }
+}
---
+++ BookListRazor/BookListRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BookListRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasKey(s => s.Id);
+            modelBuilder.Entity<Book>().Property(s => s.Name).IsRequired();
+        }
+    }
+}
